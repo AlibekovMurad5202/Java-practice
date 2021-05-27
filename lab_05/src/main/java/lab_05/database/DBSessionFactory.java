0f9019dfa7dfdb93f81bb3a70d4127888b97f5ec
AlibekovMurad5202 <MuradAlibekov2000@gmail.com>
2021-05-27 07:19:41 +0300

    lab_05 v1.0

---
+++ lab_05/src/main/java/lab_05/database/DBSessionFactory.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_05.database;
+
+import lab_05.events.listeners.alarms.AlarmHM;
+import lab_05.events.listeners.alarms.AlarmHMS;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author murad
+ */
+public class DBSessionFactory {
+    private static SessionFactory sessionFactory;
+    
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration()
+                        .configure();
+                
+                configuration.addAnnotatedClass(AlarmHM.class);
+                configuration.addAnnotatedClass(AlarmHMS.class);
+                
+                StandardServiceRegistryBuilder builder = 
+                        new StandardServiceRegistryBuilder()
+                                .applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                //System.out.println("Exception: " + e);
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
